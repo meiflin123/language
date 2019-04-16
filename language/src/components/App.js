a927ee4442970fd40d3c36cc57b1e88c16da1197
Meifang Lin <meiflin123@gmail.com>
2019-04-15 18:13:06 -0700

    create onClick event handler for selecting a language

--- language/src/components/App.js
+++ language/src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { language: 'English'};  
+
+  onClickLanguage = language => {
+    this.setState({ language: language })
+  }
+
 
   render() {
     return (
       <div className="ui container">
-        App
+        Select a language: 
+          <i className="us flag" onClick={() => this.onClickLanguage('English')}></i>
+          <i className="cn flag" onClick={() => this.onClickLanguage('Chinese')}></i>
+    
+          {this.state.language}
+
       </div>
+
+
     )
  }
 }
